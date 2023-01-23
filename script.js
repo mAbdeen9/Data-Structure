c88978a1387828e3e6596e8a97a25546f9298c81
mAbdeen9 <93052095+mAbdeen9@users.noreply.github.com>
2023-01-23 17:27:29 +0200

    graph

--- script.js
+++ script.js
@@ -315,4 +315,45 @@ const PriorityQueueBlock = () => {
   //
 };
 
-PriorityQueueBlock();
+// PriorityQueueBlock();
+
+//Graph
+class Graph {
+  constructor() {
+    this.nodes = {};
+    this.edges = {};
+  }
+
+  addNode(identifier, value) {
+    if (this.nodes[identifier]) {
+      throw new Error("Node exists already!");
+    }
+    this.nodes[identifier] = value;
+  }
+
+  addEdge(startNode, endNode) {
+    if (!this.edges[startNode] || !this.edges[endNode]) {
+      throw new Error("Start or end node does not exist!");
+    }
+    if (
+      this.edges[startNode] &&
+      this.edges[startNode].indexOf(endNode) === -1
+    ) {
+      this.edges[startNode].push(endNode);
+    } else {
+      this.edges[startNode] = [endNode];
+    }
+  }
+}
+
+const graph = new Graph();
+
+graph.addNode(1, "a");
+graph.addNode(2, "b");
+graph.addNode(3, "c");
+
+graph.addEdge(1, 2);
+graph.addEdge(1, 3);
+graph.addEdge(3, 2);
+
+console.log(graph);
